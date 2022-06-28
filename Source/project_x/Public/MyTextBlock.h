e1a58214b8299015e5d7e59a60fa9e82a732af86
fluffyza <stevo1799@gmail.om>
2022-06-28 21:21:46 +0100

    test
    test

---
+++ Source/project_x/Public/MyTextBlock.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/TextBlock.h"
+#include "MyTextBlock.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_X_API UMyTextBlock : public UTextBlock
+{
+	GENERATED_BODY()
+	//test	
+};
